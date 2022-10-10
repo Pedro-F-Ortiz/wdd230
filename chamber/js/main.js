25bd4b35e0efa1b9fbf3592bafe1926fe95108fe
Pedro Ortiz <piqui0224@gmail.com>
2022-10-10 03:17:45 -0600

    week 4

---
+++ chamber/js/main.js
@@ -0,0 +1,21 @@
+let date = new Date();
+
+let full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
+let current_date_element = document.querySelector("#date");
+current_date_element.innerHTML = date;
+
+let year = date.getFullYear();
+document.querySelector(".year").textContent = year;
+
+let currentDate = document.lastModified;
+document.querySelector(".time").textContent = currentDate;
+
+// Nav Button js
+hamburgerBtn = document.querySelector("menu-btn");
+
+hamburgerBtn.addEventListener("click", ()=> {
+    navLinks = document.querySelectorAll(".links"); 
+    navLinks.forEach(link => {
+        link.classList.toggle("show-nav");
+    })
+})
