9693da3fa8fad3ea3c07489e4d976e721ce4743c
Pedro Ortiz <piqui0224@gmail.com>
2022-10-31 15:34:25 -0600

    Wk.8 created join html

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -33,60 +33,60 @@ else {
 
 
 
-Lazy Images
-const image=document.querySelectorAll('img');
+// Lazy Images
+// const image=document.querySelectorAll('img');
 
-const options = {
-    threshold: 1,
-    rootMargin: "0px 0px 100px 0px"
-}
-const pimages = document.querySelectorAll("[data-src]");
+// const options = {
+//     threshold: 1,
+//     rootMargin: "0px 0px 100px 0px"
+// }
+// const pimages = document.querySelectorAll("[data-src]");
 
-function preloadImage(img){
-    const source =img.getAttribute("data-src");
-    if (!source) {
-        return;
-    }
-    img.src= source;
-}
+// function preloadImage(img){
+//     const source =img.getAttribute("data-src");
+//     if (!source) {
+//         return;
+//     }
+//     img.src= source;
+// }
 
-const io = new IntersectionObserver ( 
-    (entries, io) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            }
-            else {
-                preloadImage(entry.target);
-                io.unobserve(entry.target)
-            }
-        });
-    }, options);
+// const io = new IntersectionObserver ( 
+//     (entries, io) => {
+//         entries.forEach(entry => {
+//             if (!entry.isIntersecting) {
+//                 return;
+//             }
+//             else {
+//                 preloadImage(entry.target);
+//                 io.unobserve(entry.target)
+//             }
+//         });
+//     }, options);
 
-    pimages.forEach(image => {
-        io.observe(image);
-        })
+//     pimages.forEach(image => {
+//         io.observe(image);
+//         })
 
 
-Last visited time stamp
-if (!localStorage.getItem('lastvisit')) {
-    localStorage.setItem('lastvisit', Date.now());
-    document.getElementById('diff').textContent ='This is your 1st visit';
-}
-else {
-    setStyles();
-}
+// Last visited time stamp
+// if (!localStorage.getItem('lastvisit')) {
+//     localStorage.setItem('lastvisit', Date.now());
+//     document.getElementById('diff').textContent ='This is your 1st visit';
+// }
+// else {
+//     setStyles();
+// }
 
-function setStyles() {
-    let prevDate = localStorage.getItem('lastvisit');
-    let currDate = new Date('2022-10-27');
+// function setStyles() {
+//     let prevDate = localStorage.getItem('lastvisit');
+//     let currDate = new Date('2022-10-27');
 
-    let difference = currDate - prevDate;
-        console.log(difference);
-        let daysDifference = Math.floor(difference/1000/60/60/24);
+//     let difference = currDate - prevDate;
+//         console.log(difference);
+//         let daysDifference = Math.floor(difference/1000/60/60/24);
 
-    document.getElementById('diff').textContent = daysDifference;
+//     document.getElementById('diff').textContent = daysDifference;
 
-    localStorage.setItem('lastvisit', Date.now());
-}
+//     localStorage.setItem('lastvisit', Date.now());
+// }
 
