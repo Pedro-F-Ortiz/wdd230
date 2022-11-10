9951149340ee94fdbcbe94b3cd5d8b554cc9d7c1
Pedro Ortiz <piqui0224@gmail.com>
2022-11-09 19:55:49 -0700

    Wk.9 Activity Completed

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -20,14 +20,26 @@ getProphets(requestURL);
 function displayProphets(item) {
     let card = document.createElement("section");
     let h2 = document.createElement("h2");
+    let dob = document.createElement('p')
+    let pob = document.createElement('p')
     let image = document.createElement("img")
 
-    image.setAttribute("src", item.imageurl);
+    
 
     h2.textContent = item.name + " " + item.lastname; 
+    dob.textContent = item.birthdate;
+    pob.textContent = item.birthplace;
+    image.setAttribute("src", item.imageurl);
+    image.setAttribute('alt', item.name + ' ' + item.lastname);
     
 
     card.appendChild(h2);
+    card.appendChild(dob);
+    card.appendChild(pob);
     card.appendChild(image);
+
+// another way to do appendChild below.
+    // card.appendChild(h2).appendChild(dob).appendChild(pob).appendChild(image);
+
     document.querySelector(".cards").appendChild(card);
 }
