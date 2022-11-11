c28620b4b46222ff457f90cd975e6eb15b6ffc87
Pedro Ortiz <piqui0224@gmail.com>
2022-11-10 23:58:50 -0700

    images link update

--- lesson-9/js/directory.js
+++ lesson-9/js/directory.js
@@ -8,7 +8,7 @@ async function getBusiness(requestURL) {
         const jsObject = await response.json();
         console.log(jsObject);
         const business = jsObject["business"];
-        console.log(business[0].name);
+        console.log(business[0].Name);
         business.forEach(displayBusiness);
     }
 }
@@ -17,14 +17,14 @@ getBusiness(requestURL);
 
 function displayBusiness(item) {
     let card = document.createElement('section')
-    let l = document.createElement('l')
+    let l = document.createElement('img')
     let n = document.createElement('n')
     let a = document.createElement('a')
     let p = document.createElement('p')
     let w = document.createElement('w')
 
     l.setAttribute('src', item.Logo);
-    l.setAttribute('alt', item.Logo);
+    l.setAttribute('alt', item.Name);
     n.textContent = item.Name;
     a.textContent = item.Address;
     p.textContent = item.Phone;
