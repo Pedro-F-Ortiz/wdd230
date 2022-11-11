5face8c9e15482fa531a57c37c874e0c72acc73a
Pedro Ortiz <piqui0224@gmail.com>
2022-11-11 03:07:38 -0700

    added grid/ list buttons

--- lesson-9/js/directory.js
+++ lesson-9/js/directory.js
@@ -38,3 +38,15 @@ function displayBusiness(item) {
 
 document.querySelector('.cards').appendChild(card);
 }
+
+
+// Grid/ List buttons
+
+$('button').on('click',function(e) {
+    if ($(this).hasClass('grid')) {
+        $('#container ul').removeClass('list').addClass('grid');
+    }
+    else if($(this).hasClass('list')) {
+        $('#container ul').removeClass('grid').addClass('list');
+    }
+});
