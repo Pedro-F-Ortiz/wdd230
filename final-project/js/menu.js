8142d79c7a21cbb752854a1253710651b9663cbd
Pedro Ortiz <piqui0224@gmail.com>
2022-12-11 22:37:43 -0700

    fixed json file

--- final-project/js/menu.js
+++ final-project/js/menu.js
@@ -1,29 +1,29 @@
-// Breakfast/ Lunch menu cards in main
+// lunch menu cards in main
 
-const requestURL = "https://pedro-f-ortiz.github.io/wdd230/final-project/breakfast.json"
+const requestURL = "https://pedro-f-ortiz.github.io/wdd230/final-project/lunch.json"
 
-async function getBreakfast(requestURL) {
+async function getlunch(requestURL) {
     const response = await fetch(requestURL);
     console.log(response);
     if (response.ok) {
         const jsObject = await response.json();
         console.log(jsObject);
-        const breakfast = jsObject["breakfast"];
-        console.log(breakfast[0].name);
-        breakfast.forEach(displayBreakfast);
+        const lunch = jsObject["lunch"];
+        console.log(lunch[0].name);
+        lunch.forEach(displaylunch);
     }
 }
 
-getBreakfast(requestURL);
+getlunch(requestURL);
 
-function displayBreakfast(item) {
+function displaylunch(item) {
     let card = document.createElement("section")
     let dish = document.createElement("img")
     let name = document.createElement("h2")
     let ing = document.createElement("p")
     let price = document.createElement("p")
 
-    dish.setAttribute("src", item.imageurl);
+    dish.setAttribute("src", item.Dish);
     dish.setAttribute("alt", item.Name); 
     name.textContent = item.Name;
     ing.textContent = item.Ingredients;
@@ -34,5 +34,45 @@ function displayBreakfast(item) {
     card.appendChild(ing);
     card.appendChild(price);
 
-    document.querySelector(".breakfast-cards").appendChild(card);
+    document.querySelector(".lunch-cards").appendChild(card);
+}
+
+
+// Lunch menu cards from main
+
+const requestURL = "https://pedro-f-ortiz.github.io/wdd230/final-project/lunch.json"
+
+async function getLunch(requestURL) {
+    const response = await fetch(requestURL);
+    console.log(response);
+    if (response.ok) {
+        const jsObject = await response.json();
+        console.log(jsObject);
+        const lunch = jsObject["lunch"];
+        console.log(lunch[0].name);
+        lunch.forEach(displaylunch);
+    }
+}
+
+getlunch(requestURL);
+
+function displaylunch(item) {
+    let card = document.createElement("section")
+    let dish = document.createElement("img")
+    let name = document.createElement("h2")
+    let ing = document.createElement("p")
+    let price = document.createElement("p")
+
+    dish.setAttribute("src", item.Dish);
+    dish.setAttribute("alt", item.Name); 
+    name.textContent = item.Name;
+    ing.textContent = item.Ingredients;
+    price.textContent = item.Price;
+
+    card.appendChild(dish);
+    card.appendChild(name);
+    card.appendChild(ing);
+    card.appendChild(price);
+
+    document.querySelector(".lunch-cards").appendChild(card);
 }
